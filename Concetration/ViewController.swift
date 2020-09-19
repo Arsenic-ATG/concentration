b0912d4ae8ba8cea75028e8be822f5745cdb0f23
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-09-19 18:03:45 +0530

    Add the entire project on github

--- Concetration/ViewController.swift
+++ Concetration/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var flipCount = 0 {
+        didSet{
+            FlipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    
+    var emojiChoices: Array<String> = ["🎃","🎃","👻","👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
+        else{
+            print("chosen card was not in card buttons")
+        }
+    }
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    
+    @IBOutlet weak var FlipCountLabel: UILabel!
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
